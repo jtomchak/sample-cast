26c25184e22355108a751f322fb945c2ff24e693
Jesse Tomchak <jtomchak@gmail.com>
2018-06-16 20:09:29 -0700

    getting all the data now

--- src/App.js
+++ src/App.js
@@ -5,20 +5,39 @@ import Navigation from "./components/Navigation";
 import EpisodeList from "./components/EpisodeList";
 import EpisodeDetails from "./components/EpisodeDetails";
 
-import { Show } from "./services";
+import { Show, Episodes } from "./services";
 
 class App extends Component {
   state = {
     show: {},
+    episodes: [],
     loading: false,
     error: null
   };
   componentDidMount() {
-    Show.get(185226)
-      .then(result => result.json())
-      .then(payload => {
-        this.setState({
-          show: payload.show
+    //Tackling a lot here: fetching show data AND episodes of that particular show
+    //Eventually the episode id and info will be retrieved at a higher level
+    //but for now this is the 'root' of our app
+    let showId = parseInt(this.props.match.params.showId);
+    Promise.all([Show.get(showId), Episodes.all(showId)])
+      .then(values => {
+        return Promise.all([values[0].json(), values[1].json()]);
+      })
+      .then(results => ({
+        show: results[0].show,
+        episodes: results[1].episodes
+      }))
+      .then(({ show, episodes }) => {
+        //check for return error 'Not Found'
+        if (show.error) {
+          return this.setState({
+            error: show.error
+          });
+        }
+        //No Error. Successful fetch of show
+        return this.setState({
+          show: show,
+          episodes: episodes
         });
       })
       .catch(err => {
--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 //adding bootstrap & dependencies
 import "./include/bootstrap";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//App is wrapped in Router to dynamicly pass the param 'showId' as prop
+//this always App to fetch show details on init
+ReactDOM.render(
+  <Router>
+    <Route path={"/:showId"} component={App} />
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
