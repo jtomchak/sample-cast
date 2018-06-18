3585d9714593217381f937ea471db069cd5eafaa
Jesse Tomchak <jtomchak@gmail.com>
2018-06-18 11:39:23 -0700

    thumbnail in list

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -11,7 +11,12 @@ const EpisodeList = ({ episodes, onSelect, selectedEpisode }) => {
       onClick={event => onSelect(event, e.id)}
     >
       <div className="d-flex w-100 justify-content-between">
-        <h5 className="mb-1">{e.title}</h5>
+        <img
+          className="align-start img-fluid h-25 rounded img-thumbnail episode-thumbnail"
+          src={e.image_url}
+          alt="Generic placeholder image"
+        />
+        <h5 className="mb-1 pl-2">{e.title}</h5>
         <small className="d-none d-md-block">3 days ago</small>
       </div>
       <p
